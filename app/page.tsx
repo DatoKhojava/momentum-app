83d2ea17a44d35d43889d26f958d5f8dcdb30b71
datokhojava <khojavad30@gmail.com>
2025-03-13 16:03:00 +0400

    updated mai page

--- app/page.tsx
+++ app/page.tsx
@@ -1,19 +1,9 @@
 "use client"
 
-import { departmentServices } from "@/api/queries/departmentService";
-import { Button } from "@/components/ui/button";
-import { useQuery } from "@tanstack/react-query";
-
 export default function Home() {
-  const { data } = useQuery({
-    queryKey: ['todos'],
-    queryFn: departmentServices.getDepartments
-  })
-
   return (
     <div>
       <h1>💡 Momentum - Progress Tracking Software</h1>
-      <Button>Click me</Button>
     </div>
   );
 }
